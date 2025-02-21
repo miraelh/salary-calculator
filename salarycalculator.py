70aa38225d78a3811bbd9509e2fe3f45b90e14e6
Mira E <miraelhajj0@gmail.com>
2025-02-21 16:44:14 +0200

    Adding the while loop

--- salarycalculator.py
+++ salarycalculator.py
@@ -2,3 +2,10 @@ months = []
 
 
 monthname = input("Please enter the name of the month: ")
+
+while monthname!= "stop":
+    salary = int(input("Please enter your Salary for this month: "))
+
+
+
+    monthname = input("Please enter the name of the month: ")
