9047f5709349c889bac0bbc5d96309ea9933d8a3
Mira E <miraelhajj0@gmail.com>
2025-02-21 22:53:43 +0200

    Giving the option to add an additional amount

--- salarycalculator.py
+++ salarycalculator.py
@@ -39,7 +39,7 @@ while monthname!= "stop":
     print("The amount allocated for Savings for this month is: " + str(savings))
     print("The amount allocated for Rent for this month is: " + str(rent))
     print("The amount allocated for Electricity for this month is: " + str(electricity))
-    print("The total amount spent is: " + str(totalamountspent))
+    print("The total amount spent this month is: " + str(totalamountspent))
 
 
 
@@ -48,6 +48,12 @@ while monthname!= "stop":
     sumrent = sumrent + rent
     sumsavings = sumsavings + savings
 
+    action = input("Would you like to enter a an addtional amount for this month? (yes/no) ")
+    if action == "yes" :
+        additionalamount = int(input("Please enter the additional amount: "))
+        sumsavings = sumsavings + additionalamount
+        print("This added amount makes up " + str(100*additionalamount/sumsavings) + "%" + "of your total savings so far"  )
+        print("Your total amount of savings so far this year would thus be " + str(sumsavings))
 
     currentmonth = monthname
     monthname = input("Please enter the name of the month: ")
