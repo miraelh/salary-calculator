a27c1108fdd6cf76a51ca5e103156fdcc25559ee
Mira E <miraelhajj0@gmail.com>
2025-02-21 17:28:56 +0200

    Initializing and setting the sum variables to 0

--- salarycalculator.py
+++ salarycalculator.py
@@ -3,6 +3,12 @@ months = []
 
 monthname = input("Please enter the name of the month: ")
 
+
+sumelect=0
+sumrent=0
+sumsavings=0
+
+
 while monthname!= "stop":
     salary = int(input("Please enter your Salary for this month: "))
     percentagerent= int(input("Please enter the percentage allocated to Rent: "))
