8259064a30ab88eebb7202d6fa060c099bfd4a80
Mira E <miraelhajj0@gmail.com>
2025-02-21 17:09:22 +0200

    Asking for savings and electricity percentages

--- salarycalculator.py
+++ salarycalculator.py
@@ -6,6 +6,7 @@ monthname = input("Please enter the name of the month: ")
 while monthname!= "stop":
     salary = int(input("Please enter your Salary for this month: "))
     percentagerent= int(input("Please enter the percentage allocated to Rent: "))
-
+    percentagesavings= int(input("Please enter the percentage allocated to Savings: "))
+    percentageelectricity= int(input("Please enter the percentage allocated to Electricity: "))
 
     monthname = input("Please enter the name of the month: ")
