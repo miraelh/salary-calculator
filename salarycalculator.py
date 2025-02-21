fe50054b9632f18b3ee69fcb989b24abfa8f50da
Mira E <miraelhajj0@gmail.com>
2025-02-21 23:51:00 +0200

    Enhancing readability

--- salarycalculator.py
+++ salarycalculator.py
@@ -5,6 +5,7 @@ sumrent=0
 sumsavings=0
 sumremainder=0
 
+print()
 monthname = input("Please enter the name of the month: ")
 while monthname!= "stop":
     salary = int(input("Please enter your Salary for this month: "))
@@ -34,9 +35,11 @@ while monthname!= "stop":
     
     remainder = salary - totalamountspent
 
+    print()
     print("The amount allocated for Rent for this month is: " + str(rent))
     print("The amount allocated for Savings for this month is: " + str(savings))
     print("The amount allocated for Electricity for this month is: " + str(electricity))
+    print()
     print("The total amount spent this month is: " + str(totalamountspent))
     print("The remainder for this month is: " + str(remainder))
 
@@ -46,14 +49,17 @@ while monthname!= "stop":
     sumrent = sumrent + rent
     sumsavings = sumsavings + savings
     sumremainder = sumremainder + remainder
-
+    
+    print()
     action = input("Would you like to enter a an addtional amount for this month? (yes/no) ")
     if action == "yes" :
         additionalamount = int(input("Please enter the additional amount: "))
         sumsavings = sumsavings + additionalamount
+        print()
         print("This added amount makes up " + str(100*additionalamount/sumsavings) + "%" + " of your total savings so far")
         print("Your total amount of savings so far this year would thus be " + str(sumsavings))
 
+    print()
     currentmonth = monthname
     monthname = input("Please enter the name of the month: ")
 
@@ -61,12 +67,13 @@ while monthname!= "stop":
     
 # print (months)
 
+print()
 print("The total amount allocated for Savings is: " + str(sumsavings))
 print("The total amount allocated for Rent is: " + str(sumrent))
 print("The total amount allocated for Electricity is: " + str(sumelect))
 print("The total remainder is: " + str(sumremainder))
-
+print()
 print(f"The estimate yearly Rent based on the month of {currentmonth} is: " + str(rent*12))
 print(f"The estimate yearly Electricity based on the month of {currentmonth} cost is: " + str(electricity*12))
-
+print()
 print(f"Fun fact, your salary for the month of {currentmonth} raised to the power of 2 is: " + str(pow(salary,2)))
