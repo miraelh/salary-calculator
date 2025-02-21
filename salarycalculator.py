cfe09a21208f8b391bcc0d611d8bd882e8483c81
Mira E <miraelhajj0@gmail.com>
2025-02-21 16:52:43 +0200

    Asking for rent percentage

--- salarycalculator.py
+++ salarycalculator.py
@@ -5,7 +5,7 @@ monthname = input("Please enter the name of the month: ")
 
 while monthname!= "stop":
     salary = int(input("Please enter your Salary for this month: "))
-
+    percentagerent= int(input("Please enter the percentage allocated to Rent: "))
 
 
     monthname = input("Please enter the name of the month: ")
